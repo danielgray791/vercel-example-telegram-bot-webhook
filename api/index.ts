78919352bddaec5fd29608d82c2b1e83e9b7e550
danielgray791 <danielgray791@outlook.com>
2025-03-27 00:14:33 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,37 @@
-import { botInstance } from './handler/bot';
 import express, { Express, Request, Response } from "express";
 import axios from 'axios';
+import TelegramBot from 'node-telegram-bot-api';
 
+const TOKEN = process.env.TOKEN;
+const PORT = process.env.PORT;
+
+// Initialize Bot Instance
+const bot = new TelegramBot(TOKEN, {webHook: true});
+process.env.NTBA_FIX_350 = "true";
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, 'Received your message');
+});
+
+// Initialize Webhook
 const app: Express = express();
 app.use(express.json())
 
@@ -20,8 +50,8 @@ app.get("/", async (req, res) => {
 app.get("/setwebhook", async (req, res) => {
     const baseUrl = `https://${req.hostname}`;
     try{
-        await botInstance.deleteWebHook();
-        await botInstance.setWebHook(`${baseUrl}/fetch-updates`);
+        await bot.deleteWebHook();
+        await bot.setWebHook(`${baseUrl}/fetch-updates`);
         res.status(200).send({
             error: null,
             message: `Webhook successfully set to ${baseUrl}`
@@ -37,7 +67,7 @@ app.get("/setwebhook", async (req, res) => {
 app.get("/delwebhook", async (req, res) => {
     const baseUrl = `https://${req.hostname}`;
     try{
-        await botInstance.deleteWebHook();
+        await bot.deleteWebHook();
         res.status(200).send({
             error: null,
             message: `Webhook successfully delete from ${baseUrl}`
@@ -65,7 +95,7 @@ app.post("/fetch-updates", async (req, res) => {
 app.post("/process-updates", async (req, res) => {
     try {
         await new Promise<void>((resolve, reject) => {
-            botInstance.processUpdate(req.body);
+            bot.processUpdate(req.body);
 
             setTimeout(() => {
                 console.log("Resolved");
@@ -83,8 +113,8 @@ app.post("/process-updates", async (req, res) => {
     res.status(200).send({error: null, data: req.body});
 })
 
-app.listen(3000, () => { 
-    console.log("Server ready on port 3000.")
+app.listen(PORT, () => { 
+    console.log(`Server ready on port ${PORT}.`)
 });
 
 module.exports = app;
